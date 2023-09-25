397694c652526ca281df9928ff4e2328308d419e
Sonu Verma <sonuv@infinite.com>
2023-09-25 13:19:10 +0530

    CodeBase_3

---
+++ CodeBased Test/CodeBase_3/CodeBaseTest_3/Question_1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeBaseTest_3
+{
+    class Cricket
+    {
+        public void Pointscalculation(int no_of_Matches)
+        {
+            //create a list to store the scores for each match
+            List<double> scores = new List<double>();
+            //loop  to input score
+            for (int i = 0; i < no_of_Matches; i++)
+            {
+                Console.Write($"Enter score for Match {i + 1}: ");
+                //to parse the user input (Score)
+                if (double.TryParse(Console.ReadLine(), out double score))
+                {
+                    scores.Add(score);// add the score to the list
+                }
+                else
+                {
+                    //Display error message for wrong input and retry the match
+                    Console.WriteLine("Invalid input. Please enter a valid number.");
+                    i--; //Decrement i to re-enter the score
+                }
+            }
+            // calculate the total score by summing up all the scores 
+            double totalScore = 0;
+            foreach (double score in scores)
+            {
+                totalScore += score;
+            }
+            // calculate the Avg score
+            double averageScore = totalScore / no_of_Matches;
+
+            // display the total score of all matches
+            Console.WriteLine($"Total Score: {totalScore}");
+            Console.WriteLine($"Average Score: {averageScore}");
+        }
+
+        static void Main(string[] args)
+        {
+            Cricket cricketGame = new Cricket();
+
+            Console.Write("Enter the number of Matches: ");
+
+            //try to parse th user input as an integer(no.of matches)
+            if (int.TryParse(Console.ReadLine(), out int no_of_Matches))
+            {
+                cricketGame.Pointscalculation(no_of_Matches);
+            }
+            else
+            {
+                //display error message
+                Console.WriteLine("Invalid input. Please enter a valid number of Matches.");
+                
+            }
+            Console.ReadLine();
+        }
+    }
+        
+}
+   
---
+++ CodeBased Test/CodeBase_3/Question_2/Question_2.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Question_2
+{
+    class Box
+    {
+       // Members
+    public double Length { get; set; }
+        public double Breadth { get; set; }
+
+        // Constructor
+        public Box(double length, double breadth)
+        {
+            Length = length;
+            Breadth = breadth;
+        }
+
+        // Function to add two Box instance and store the result in a third Box
+        public static Box AddBoxes(Box box1, Box box2)
+        {
+            double newLength = box1.Length + box2.Length;
+            double newBreadth = box1.Breadth + box2.Breadth;
+            return new Box(newLength, newBreadth);
+        }
+
+        // Display the details of the Box
+        public void DisplayDetails()
+        {
+            Console.WriteLine($"Length: {Length} units");
+            Console.WriteLine($"Breadth: {Breadth} units");
+        }
+    }
+
+    class Test
+    {
+        static void Main(string[] args)
+        {
+            // Create two Box Instance
+            Box box1 = new Box(19.99, 12.0);
+            Box box2 = new Box(67.5, 91.5);
+
+            // Add the two Box instance and store the result in a third Box
+            Box box3 = Box.AddBoxes(box1, box2);
+
+            // Display the details of the third Box
+            Console.WriteLine("Details of the Third Box:");
+            Console.Write("_________________________\n");
+            box3.DisplayDetails();
+            Console.ReadLine();
+        }
+    }
+}
