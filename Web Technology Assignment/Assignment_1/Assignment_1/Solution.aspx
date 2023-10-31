1ca4f57ee91a6cae676a189c24d587c62965191e
Sonu Verma <sonuv@infinite.com>
2023-10-31 16:56:19 +0530

    Web Technology Assignment

---
+++ Web Technology Assignment/Assignment_1/Assignment_1/Solution.aspx
@@ -0,0 +1,31 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Solution.aspx.cs" Inherits="Assignment_1.Solution" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Car Store</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <h1>Car Store</h1>
+            <asp:DropDownList ID="ddlCars" runat="server" AutoPostBack="true" OnSelectedIndexChanged="ddlCars_SelectedIndexChanged">
+   <asp:ListItem Text="Select a Car" Value="" />
+                <asp:ListItem Text="Sedan" Value="sedan" />
+                <asp:ListItem Text="SUV" Value="suv" />
+                <asp:ListItem Text="Sports Car" Value="sports" />
+            </asp:DropDownList>
+</asp:DropDownList>
+            <br />
+            <br />
+            <asp:Image ID="imgCar" runat="server" Width="300" Height="200" />
+            <br />
+            <asp:Button ID="btnShowCost" runat="server" Text="Show Cost" OnClick="btnShowCost_Click" />
+            <br />
+            <br />
+            <asp:Label ID="lblCost" runat="server" Text=""></asp:Label>
+        </div>
+    </form>
+</body>
+</html>
---
+++ Web Technology Assignment/Assignment_1/Assignment_1/Solution.aspx.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Assignment_1
+{
+    public partial class Solution : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                // Initialize the DropDownList
+                ddlCars.SelectedIndex = 0;
+            }
+        }
+
+        protected void ddlCars_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            // Load the corresponding image when a car is selected
+            string selectedCar = ddlCars.SelectedValue;
+            switch (selectedCar)
+            {
+                case "sedan":
+                    imgCar.ImageUrl = "Images/sedan/BMW 5 Series.jpg"; // Relative path to the image
+                    break;
+                case "suv":
+                    imgCar.ImageUrl = "Images/SUV/Mahindra Thar.jpg"; // Relative path to the image
+                    break;
+                case "sports":
+                    imgCar.ImageUrl = "Images/Sports Car/Mustang.jpg"; // Relative path to the image
+                    break;
+                default:
+                    imgCar.ImageUrl = ""; // Clear the image
+                    break;
+            }
+        }
+
+        protected void btnShowCost_Click(object sender, EventArgs e)
+        {
+            // Display the cost of the selected car
+            string selectedCar = ddlCars.SelectedValue;
+            string cost = "";
+            switch (selectedCar)
+            {
+                case "sedan":
+                    cost = "5000000.";
+                    break;
+                case "suv":
+                    cost = "2500000.";
+                    break;
+                case "sports":
+                    cost = "10000000.";
+                    break;
+                default:
+                    cost = "0.";
+                    break;
+            }
+            lblCost.Text = "Cost: " + cost;
+        }
+    }
+}
