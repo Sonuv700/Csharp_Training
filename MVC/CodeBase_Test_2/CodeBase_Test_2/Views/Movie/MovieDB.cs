5bc573b8ed60eac7a4dcb0e16470fecc9f3e5ee4
Sonu Verma <sonuv@infinite.com>
2023-11-14 13:38:05 +0530

    CodeBase_Test_2

---
+++ MVC/CodeBase_Test_2/CodeBase_Test_2/Views/Movie/MovieDB.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace CodeBase_Test_2.Views.Movie
+{
+    public class MoviesDB : DbContext
+    {
+        public MoviesDB() : base("MoviesDB")
+
+        {
+
+        }
+        public DbSet<Movies> Movie { get; set; }
+
+    }
+}
---
+++ MVC/CodeBase_Test_2/CodeBase_Test_2/Views/Movie/Movies.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace CodeBase_Test_2.Views.Movie
+{
+    public class Movies
+    {
+        [Key]
+        public int Mid { get; set; }
+
+        [Required]
+        public string Moviename { get; set; }
+
+        [Required]
+        public DateTime DateofRelease { get; set; }
+    }
+}
